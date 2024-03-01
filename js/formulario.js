fa2cdc9d94339b9c88bb074dd2eabd96c6619080
Ivonne Cantor <ivonnecp@gmail.com>
2024-03-01 16:35:28 +0100

    aplicando conceptos de formularios

---
+++ js/formulario.js
@@ -0,0 +1,33 @@
+function reservar(){
+    //capturamos los datos del formulario
+    let nombre = document.getElementById("nombre").value;
+    let apellidos = document.getElementById("apellidos").value;
+    let adultos= document.getElementById("adultos").value;
+    let ninos = document.getElementById("ninos").value;
+    let regimen = document.getElementsByName("regimen");
+
+    //recorremos regimen para saber cual tiene el check
+    for(let i = 0; i < regimen.length; i++){
+        if(regimen[i].checked == true){
+            regimen = regimen[i].id
+            break
+        }
+    }
+
+    //empezamos a corregir errores de los clientes al introducir datos
+    
+
+
+
+
+
+
+
+    alert(`Datos de la reserva:
+    nombre: ${nombre}
+    apellidos: ${apellidos}
+    adultos: ${adultos}
+    niños: ${ninos}
+    comidas: ${regimen}`)
+}
+
